74ccfbd8abdb6d4e1eb2dd29cf2e790f1ced7050
Gustavo <gs.cesario1998@gmail.com>
2024-03-17 18:01:02 -0300

    Initial commit

---
+++ src/main/java/app/cesario/springecommerce/SpringEcommerceApplication.java
@@ -0,0 +1,13 @@
+package app.cesario.springecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceApplication.class, args);
+	}
+
+}
